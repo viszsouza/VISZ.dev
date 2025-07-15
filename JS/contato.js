3592fac0a322e60902d7a41da6ae61f04cb81ecc
vizssouza <vini.nelso@gmail.com>
2025-07-14 22:04:34 -0300

    Adicionando medias querys, e aniamções
    Ainda falta adicionar o rodapé das páginas

---
+++ JS/contato.js
@@ -0,0 +1,5 @@
+// CONTATO LOADING
+const buttonContato = document.querySelector("#button-contato")
+const loader = document.querySelector(".loader")
+
+buttonContato.addEventListener("click", () => loader.style.display = "block")
--- JS/scripts.js
+++ JS/scripts.js
@@ -5,3 +5,25 @@ const navlist = document.querySelector(".nav-list")
 
 navlist.addEventListener("click", () => nav.classList.toggle("active"));
 hamburguer.addEventListener("click", () => nav.classList.toggle("active"));
+
+// ANIMAÇÕES GLOBAIS
+// Coletando o observador da página
+const myObserver = new IntersectionObserver( (entries) => {
+    entries.forEach( (entry) => {
+        if (entry.isIntersecting === true){
+            entry.target.classList.add('show')
+        } 
+    })
+})
+
+// Selecionando os elementos com a classe ".hidden"
+const elements = document.querySelectorAll('.hidden')
+
+// Selecionando 1 arquivos por vez da classe ".hidden"
+elements.forEach( (element) => myObserver.observe(element))
+
+// Selecionando os elementos com a classe ".hidden"
+const elements2 = document.querySelectorAll('.hidden2')
+
+// Selecionando 1 arquivos por vez da classe ".hidden"
+elements2.forEach( (element) => myObserver.observe(element))
